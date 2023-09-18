34a2237fd32c872175c2cb6444e65666753e2234
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2023-09-18 17:27:43 -0300

    trabajo de position

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 // import { ContadorScreen } from './src/screens/ContadorScreen';
-import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+// import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 import { SafeAreaView } from 'react-native';
-import { DimensionesScreen } from './src/screens/DimensionesScreen';
+import { PositionScreen } from './src/screens/PositionScreen';
+// import { DimensionesScreen } from './src/screens/DimensionesScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
 const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1}}>
-    {/*  <HolaMundoScreen />
-     <ContadorScreen /> */}
-    {/* <BoxObjectModelScreen /> */}
-    <DimensionesScreen />
+    <SafeAreaView style={{flex: 1}}>
+    {/* // <HolaMundoScreen />
+    // <ContadorScreen />  
+    // <BoxObjectModelScreen />  
+    // <DimensionesScreen />   */}
+    <PositionScreen />
     </SafeAreaView>
   );
 };
---
+++ 02-MiPrimeraApp/src/screens/PositionScreen.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { StyleSheet, View } from 'react-native'
+
+export const PositionScreen = () => {
+  return (
+    <View style={ styles.container }>
+        <View style={ styles.cajaVerde }/>
+        <View style={ styles.cajaMorada }/>
+        <View style={ styles.cajaNaranja }/>
+    </View>
+  )
+}
+    const styles = StyleSheet.create({
+        container:{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#28C4D9',
+            // width: 400,
+            // height: 400,
+        },
+        cajaMorada: {
+            width: 100,
+            height: 100,
+            backgroundColor: '#5856D6',
+            borderWidth: 10,
+            borderColor: 'white',
+            position: 'absolute',
+            top: 10,
+            right: 10,
+        },
+        cajaNaranja: {
+            width: 100,
+            height: 100,
+            backgroundColor: '#F0A23B',
+            borderWidth: 10,
+            borderColor: 'white',
+            position: 'absolute',
+            bottom: 10,
+            right: 10
+        },
+        cajaVerde: {
+            // width: 100,
+            // height: 100,
+            backgroundColor: 'green',
+            borderWidth: 10,
+            borderColor: 'white',
+            // position: 'absolute',
+            // bottom: 0,
+            // left: 0,
+            // top: 0,
+            // right: 0,
+            ...StyleSheet.absoluteFillObject // esto es lo mismo que lo de asrriba, desestructura StyleSheet
+        },
+    });
