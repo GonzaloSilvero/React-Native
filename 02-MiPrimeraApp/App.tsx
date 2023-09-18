2bbfe08c603bd43c0d99859a1943f1eb29d48aca
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2023-09-18 14:07:29 -0300

    dimensiones de la pantalla en tiempo real

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 // import { ContadorScreen } from './src/screens/ContadorScreen';
 import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
 import { SafeAreaView } from 'react-native';
+import { DimensionesScreen } from './src/screens/DimensionesScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
 const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1}}>
     {/*  <HolaMundoScreen />
      <ContadorScreen /> */}
-    <BoxObjectModelScreen />
+    {/* <BoxObjectModelScreen /> */}
+    <DimensionesScreen />
     </SafeAreaView>
   );
 };
