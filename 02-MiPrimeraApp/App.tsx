4751fe5b438322ce251f0a16e62bafe1efa91b93
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2023-09-18 10:17:28 -0300

    inicio de seccion de design

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { ContadorScreen } from './src/screens/ContadorScreen';
+// import { ContadorScreen } from './src/screens/ContadorScreen';
+import { BoxObjectModelScreen } from './src/screens/BoxObjectModelScreen';
+import { SafeAreaView } from 'react-native';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
 const App = () => {
   return (
-    // <HolaMundoScreen />
-    <ContadorScreen />
+    <SafeAreaView>
+    {/*  <HolaMundoScreen />
+     <ContadorScreen /> */}
+    <BoxObjectModelScreen />
+    </SafeAreaView>
   );
 };
 
