f56af182441f398fe16e3966a9c20fb93717739d
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2023-09-28 14:18:13 -0300

    tarea 1

--- 02-MiPrimeraApp/App.tsx
+++ 02-MiPrimeraApp/App.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import { SafeAreaView } from 'react-native';
 import { PositionScreen } from './src/screens/PositionScreen';
 import { FlexScreen } from './src/screens/FlexScreen';
+import { TareaScreen } from './src/screens/TareaScreen';
 // import { DimensionesScreen } from './src/screens/DimensionesScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
@@ -15,7 +16,8 @@ const App = () => {
     // <BoxObjectModelScreen />  
     // <DimensionesScreen />   */}
     {/* <PositionScreen /> */}
-    <FlexScreen/>
+    {/* <FlexScreen/> */}
+    <TareaScreen />
     </SafeAreaView>
   );
 };
