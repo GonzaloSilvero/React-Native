2e0fe9efe4671217ff1b6ae05f25aa52425a72bf
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2023-10-02 17:55:22 -0300

    terminada la calculadora con sus operaciones y reformulacion con custom hook

--- 03-calculadora/src/screens/CalculadoraScreen.tsx
+++ 03-calculadora/src/screens/CalculadoraScreen.tsx
@@ -1,105 +1,26 @@
-import React, { useRef, useState } from 'react'
-import { Text, View, StatusBar } from 'react-native';
+import React from 'react'
+import { Text, View } from 'react-native';
 import { styles } from '../theme/appTheme'
 import { BotonCal } from '../components/BotonCal'
+import { useCalculadora } from '../hooks/useCalculadora';
 
-enum Operadores {
-    sumar, restar, multiplar, dividir
-}
+
 
 export const CalculadoraScreen = () => {
-
-    const [numeroAnterior, setNumeroAnterior] = useState('0')
-    const [numero, setNumero] = useState('0')
-
-    const ultimaOperacion = useRef<Operadores>()
-
-    const limpiar = () => {
-        setNumero('0');
-        setNumeroAnterior('0');
-    }
-
-    const armarNumero = ( numeroTexto: string ) => {
-        
-        //no aceptar doble punto
-        if ( numero.includes('.') && numeroTexto === '.' ) return;
-
-        if ( numero.startsWith('0') || numero.startsWith('-0') ) {
-            
-            //punto decimal
-            if ( numeroTexto === '.' ) {
-                setNumero( numero + numeroTexto )
-                
-                //evaluar si es otro cero, y hay un punto
-            } else if ( numeroTexto === '0' && numero.includes('.') ) {
-                setNumero( numero + numeroTexto )
-
-                // evaluar si es diferente de 0 y no tiene un punto
-            } else if ( numeroTexto !== '0' && !numero.includes('.') ) {
-                setNumero( numeroTexto )
-                
-                //evitar 0000.0
-            } else if ( numeroTexto === '0' && !numero.includes('.') ) {
-                setNumero( numero )
-            } else {
-                setNumero( numero + numeroTexto )
-            }
-
-        } else {
-            setNumero( numero + numeroTexto )
-        }
-
-    }
-
-    const positivoNegativo = () => {
-        if( numero.includes('-') ) {
-            setNumero( numero.replace('-', '') )
-        } else {
-            setNumero( '-' + numero )
-        }
-    }
-
-    const btnDelete = (  ) => {
-
-        let negativo = '';
-        let numeroTemp = numero;
-        if ( numero.includes('-') ) {
-            negativo = '-';
-            numeroTemp = numero.substr(1);
-        }
-
-        if ( numeroTemp.length > 1 ) {
-            setNumero( numeroTemp = numeroTemp.slice(0,-1) );
-        } else {
-            setNumero('0');
-        }
-    }
-
-    const cambiarNumPorAnterior = () => {
-        if ( numero.endsWith('.') ) {
-            setNumeroAnterior( numero.slice(0,-1) );
-        } else {
-            setNumeroAnterior( numero );
-        }
-        setNumero('0')
-    }
-
-    const btnDividir = () => {
-        cambiarNumPorAnterior();
-        ultimaOperacion.current = Operadores.dividir;
-    }
-    const btnMultiplicar = () => {
-        cambiarNumPorAnterior();
-        ultimaOperacion.current = Operadores.multiplar;
-    }
-    const btnRestar = () => {
-        cambiarNumPorAnterior();
-        ultimaOperacion.current = Operadores.restar;
-    }
-    const btnSumar = () => {
-        cambiarNumPorAnterior();
-        ultimaOperacion.current = Operadores.sumar;
-    }
+    
+    const { 
+        numero, 
+        numeroAnterior, 
+        limpiar, 
+        positivoNegativo, 
+        armarNumero, 
+        calcular, 
+        btnDelete, 
+        btnDividir, 
+        btnMultiplicar, 
+        btnRestar, 
+        btnSumar 
+    } = useCalculadora()
 
   return (
     <View style={ styles.calculadoraContainer }>
@@ -163,7 +84,7 @@ export const CalculadoraScreen = () => {
             {/* Boton */}
             <BotonCal texto='0' ancho accion={ armarNumero }/>
             <BotonCal texto='.' accion={ armarNumero }/>
-            <BotonCal texto='=' color='#FF9427' accion={ limpiar }/>
+            <BotonCal texto='=' color='#FF9427' accion={ calcular }/>
 
         </View>
 
