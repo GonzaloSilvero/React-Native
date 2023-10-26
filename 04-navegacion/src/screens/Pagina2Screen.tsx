c609f723f024c1cc79722984d7a675cad6c1e8dc
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2023-10-26 08:49:12 -0300

    sobre escribir lo que viene por defecto por el padre en srcreens especificas

--- 04-navegacion/src/screens/Pagina2Screen.tsx
+++ 04-navegacion/src/screens/Pagina2Screen.tsx
@@ -1,11 +1,19 @@
 import { View, Text, Button } from 'react-native';
-import React from 'react'
+import React, { useEffect } from 'react'
 import { styles } from '../theme/appTheme'
 import { useNavigation } from '@react-navigation/native';
 
 export const Pagina2Screen = () => {
 
   const navigator = useNavigation();
+
+  useEffect(() => { //esto sobre escribe el predeterminado del padre
+    navigator.setOptions({
+      title: 'Hola Mundo',
+      headerBackTitle: 'Atras'
+    })
+  }, [])
+  
 
   return (
     <View style={ styles.globalMargin }>
