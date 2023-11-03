309d6cf8fc7cb8d512314c981a94c1a47e2ac234
Gonzalo Silvero <gonzalosilvero02@gmail.com>
2023-11-03 08:07:51 -0300

    integracion de iconos e implementacion de uno

--- 04-navegacion/src/screens/Tab1Screen.tsx
+++ 04-navegacion/src/screens/Tab1Screen.tsx
@@ -1,16 +1,25 @@
-import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import { View, Text } from 'react-native'
+import { styles } from '../theme/appTheme'
+
 
 export const Tab1Screen = () => {
-
+  
   useEffect(() => {
     console.log('Tab1Screen effect')
   }, [])
   
-
+  
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={ styles.globalMargin }>
+      <Text style={ styles.title }>iCONOS</Text>
+
+      {/* los iconos van dentro de textos */}
+      <Text>
+        <Icon name="airplane-outline" size={30} color="#900" />;
+      </Text>
     </View>
   )
 }
